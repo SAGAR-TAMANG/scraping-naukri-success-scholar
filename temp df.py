d9e82fe7527919cb904b066c6a26179f482be295
Sagar Tamang <sagar.bdr0000@gmail.com>
2023-07-07 17:11:05 +0530

    final touch

--- temp df.py
+++ temp df.py
@@ -1,25 +1,42 @@
+import requests
+from selenium import webdriver
+from bs4 import BeautifulSoup
+import time
 import pandas as pd
-
-l1 = [1,2,3,4,5]
-l2 = [1,3,5,7,9]
-
-df1 = pd.DataFrame()
-df1['Column 1'] = l1
-df1['Column 2'] = l2
-
-# df2 = pd.DataFrame()
-# df2['Column 1'] = l2
-# df2['Column 2'] = l1
-
-df3 = pd.DataFrame([['THIS IS A TEXT', 2], ['ANOTHER TEXT', 4]], columns = ['First column', 'Second'])
-df2 = pd.DataFrame([[5, 6], [7, 8]], columns= ['First column', 'Second'])
-
-df3 = pd.concat([df3, pd.DataFrame([[5,6]], columns = ['First column', 'Second'])], ignore_index=True)
-# df = pd.concat([df, pd.DataFrame([new_row])], ignore_index=True)
+import numpy as np
+from selenium.webdriver.common.by import By
+from selenium.webdriver.chrome.options import Options
 
 
-# print(df1)
-# print(' '*2)
-# print(df1.head())
+# pages = int(input('NUMBER OF PAGES WORTH OF DATA: '))
 
-print(df3)
+dff = pd.DataFrame(columns=['Job Title','Posted', 'Company','URL'])
+
+url = "https://www.naukri.com/jobs-in-india"
+# Observation: Page1: https://www.naukri.com/it-jobs Page2: https://www.naukri.com/it-jobs-2
+
+# page = requests.get(url)
+# print(page.text)
+
+# Making the Driver Headless: (next 3 lines)
+driver = webdriver.Chrome()
+# options = Options()
+# options.add_argument("--headless=new")
+# driver = webdriver.Chrome(options=options)
+
+# time.sleep(3)
+
+# driver = webdriver.Chrome()
+driver.get(url)
+
+soup = BeautifulSoup(driver.page_source,'html5lib')
+
+# print(soup.prettify())
+
+results = soup.find(class_='list')
+job_elems=results.find_all('article', class_='jobTuple')
+
+# print(results)
+# print(results.text)
+print(job_elems)
+driver.close()
